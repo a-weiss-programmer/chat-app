67ef034342ef0ac5e26f93d0cd6b51bcd1bfc1bf
caseylargent <caseylargent@unomaha.edu>
2017-11-30 18:27:41 -0600

    Refactored code into classes. Added better timestamp functionality.

--- ServerSocket.js
+++ ServerSocket.js
@@ -35,10 +35,14 @@ class Server {
         });
     }
 
+    formatMessage(nickname, msg) {
+        return `${utils.getTimestamp()} ${nickname}: ${msg}`;
+    }
+
     setSocketEvents(socketObj) {
         const self = this;
 
-        socketObj.socket.on('data', function (data) {
+        socketObj.socket.on('data', (data) => {
             const message = data.toString();
 
             if (message[0] == '/' && message.length > 1) {
@@ -47,6 +51,7 @@ class Server {
                 self.processCommand(socketObj, command, argument);
             }
             else {
+                message = self.formatMessage(socketObj.nickname, message);
                 self.broadcast(socketObj.id, message);
                 // Log it to the server output
                 console.log(message);
@@ -55,31 +60,40 @@ class Server {
 
 
         // When client leaves
-        socketObj.socket.on('end', function (socket) {
+        socketObj.socket.on('end', (socket) => {
             
-            // Since this checks for general disconnects
-            // A special case is in place for reaching 
-            // The max number of connections
-            if (self.size < self.MAX_NUM_CONNECTIONS) {
+            // Since this checks for disconnects of any kind
+            // A special case is in place for automatically
+            // disconnecting sockets when the room is at capacity.
+            if (self.numConnections <= self.MAX_NUM_CONNECTIONS) {
+                let timeStamp = utils.getTimestamp();
+                let message = `${timeStamp} ${socketObj.nickname} left the room`;
                 // Remove client from socket array
-                self.removeSocket(socket);
+                self.removeSocket(socketObj);
 
                 // Notify all clients
-                self.broadcast(socketObj.id, "Someone left the server!");
+                self.broadcast(socketObj.id, message);
+
+                // Log messages
+                console.log(message);
             }
-            
         });
+
     }
     processCommand(socketObj, command, argument) {
+        let message;
+        let timeStamp = utils.getTimestamp();
         switch (command) {
             case 'nick':
                 if (socketObj.nickname !== 'PLACEHOLDER') {
-                    this.broadcast(socketObj.id, `${socketObj.nickname} changed their nickname to ${argument}`);
+                    message = `${timeStamp} ${socketObj.nickname} changed their nickname to ${argument}`;
                 }
                 else {
-                    this.broadcast(socketObj.id,  `${argument} joined the room`);
+                    message = `${timeStamp} ${argument} joined the room`;
                 }
-                console.log(`${utils.getTimestamp()} ${argument} joined the room`);
+                
+                this.broadcast(socketObj.id, message);
+                console.log(`${message}`);
                 socketObj.nickname = argument;
                 break;
             // TODO: ADD MORE COMMANDS
@@ -89,34 +103,38 @@ class Server {
     }
     broadcast(senderId, message) {
         if (this.sockets.length === 0) {
-            console.log('No users in the chat');
+            console.log(`${utils.getTimestamp()} No users in the chat`);
             return; 
         }
 
-        this.sockets.forEach((socket) => {
-            if (socket.id === senderId) return;
-            socket.socket.write(message);
+        this.sockets.forEach((client) => {
+            if (client.id === senderId) return;
+            client.socket.write(message);
         });
     }
 
     // Remove disconnected client from sockets array
     removeSocket(socket) {
         this.sockets.splice(this.sockets.indexOf(socket), 1);
+        this.numConnections--;
     }
 }
 
-process.stdout.write('Please enter the room capacity\n> ');
-scanner.on('line', (capacity) => {
-    let size = Number.parseInt(capacity);
+function main() {
+    process.stdout.write('Please enter the room capacity\n> ');
+    scanner.on('line', (capacity) => {
+        let size = Number.parseInt(capacity);
 
-    if (isNaN(size)) {
-        process.stdout.write('Please enter a valid integer\n> ');
-    }
-    else {
-        scanner.close();
-        process.stdin.destroy();
-        new Server(size);
-    }
-});
+        if (isNaN(size)) {
+            process.stdout.write('Please enter a valid integer\n> ');
+        }
+        else {
+            scanner.close();
+            process.stdin.destroy();
+            new Server(size);
+        }
+    });
+}
 
+main();
 
